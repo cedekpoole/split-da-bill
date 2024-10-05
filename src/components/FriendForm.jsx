36769e76344e657a53e415993418434f618e3441
cedekpoole <cameron.edek.poole@gmail.com>
2024-10-05 18:16:30 +0100

    add functionality to friend form

--- src/components/FriendForm.jsx
+++ src/components/FriendForm.jsx
@@ -1,4 +1,33 @@
-export default function FriendForm({ addFriend, setAddFriend }) {
+import defaultAvatar from "../assets/no-profile-picture-15257.png";
+import { v4 as uuidv4 } from "uuid";
+import { useState } from "react";
+
+export default function FriendForm({
+  addFriend,
+  setAddFriend,
+  friends,
+  setFriends,
+}) {
+  const [name, setName] = useState("");
+  const [image, setImage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const newFriend = {
+      id: uuidv4(),
+      name,
+      image: image || defaultAvatar,
+      total: 0,
+    };
+
+    setFriends([...friends, newFriend]);
+    setAddFriend(false);
+
+    setName("");
+    setImage("");
+  };
+
   return (
     <div className="bg-[#f1f1f1] shadow-md rounded-lg max-w-xs flex flex-col">
       <div
@@ -7,15 +36,19 @@ export default function FriendForm({ addFriend, setAddFriend }) {
       >
         x
       </div>
-      <form className="flex flex-col space-y-4 p-4">
+      <form className="flex flex-col space-y-4 p-4" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="name" className="text-lg font-semibold mr-4">
             Name:
           </label>
           <input
+            name="name"
             type="text"
             className="border rounded px-4 py-2"
             placeholder="Enter friend's name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
           />
         </div>
         <div>
@@ -23,9 +56,12 @@ export default function FriendForm({ addFriend, setAddFriend }) {
             Image:
           </label>
           <input
+            name="image"
             type="url"
             className="border rounded px-4 py-2"
             placeholder="Enter image url"
+            value={image}
+            onChange={(e) => setImage(e.target.value)}
           />
         </div>
         <div className="flex justify-end">
