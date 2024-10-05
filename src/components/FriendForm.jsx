613450720880b68a1d6ac2750c9047c7a62b7010
cedekpoole <cameron.edek.poole@gmail.com>
2024-10-05 13:26:15 +0100

    edit friend form UI

--- src/components/FriendForm.jsx
+++ src/components/FriendForm.jsx
@@ -1,22 +1,34 @@
 export default function FriendForm() {
   return (
-    <form className="flex flex-col space-y-4 p-4 bg-white shadow-md rounded-lg">
-      <input
-        type="text"
-        className="border rounded px-4 py-2"
-        placeholder="Enter friend's name"
-      />
-      <input
-        type="url"
-        className="border rounded px-4 py-2"
-        placeholder="Enter image url"
-      />
-      <button
-        type="submit"
-        className="bg-blue-500 text-white py-2 rounded hover:bg-blue-600"
-      >
-        Add Friend
-      </button>
+    <form className="flex flex-col space-y-4 p-4 bg-[#f1f1f1] shadow-md rounded-lg max-w-xs">
+      <div>
+        <label htmlFor="name" className="text-lg font-semibold mr-4">
+          Name:
+        </label>
+        <input
+          type="text"
+          className="border rounded px-4 py-2"
+          placeholder="Enter friend's name"
+        />
+      </div>
+      <div>
+        <label htmlFor="image" className="text-lg font-semibold mr-4">
+          Image:
+        </label>
+        <input
+          type="url"
+          className="border rounded px-4 py-2"
+          placeholder="Enter image url"
+        />
+      </div>
+      <div className="flex justify-end">
+        <button
+          type="submit"
+          className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600 w-36"
+        >
+          Add New Friend
+        </button>
+      </div>
     </form>
   );
 }
