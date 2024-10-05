3bfb7696da9ca8f924ef125fd869165b23136565
cedekpoole <cameron.edek.poole@gmail.com>
2024-10-05 14:04:27 +0100

    fix conditional render of friend form

--- src/components/FriendList.jsx
+++ src/components/FriendList.jsx
@@ -6,9 +6,9 @@ export default function FriendList({ friends, setFriends }) {
   const [addFriend, setAddFriend] = useState(false);
   return (
     <>
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 mb-4">
         {friends.length === 0 && (
-          <p className="text-center text-lg font-semibold mt-4 max-w-xs">
+          <p className="text-center text-lg font-semibold mt-4 max-w-xs border shadow-md">
             No friends to display
           </p>
         )}
@@ -16,15 +16,18 @@ export default function FriendList({ friends, setFriends }) {
           <FriendCard key={friend.id} friend={friend} />
         ))}
       </div>
-      <div className="py-2 flex justify-end max-w-xs">
-        <button
-          className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600 w-36"
-          onClick={() => setAddFriend(!addFriend)}
-        >
-          Add Friend
-        </button>
-      </div>
-      {addFriend && <FriendForm friends={friends} setFriends={setFriends} />}
+      {addFriend ? (
+        <FriendForm friends={friends} setFriends={setFriends} />
+      ) : (
+        <div className="py-2 flex justify-end max-w-xs">
+          <button
+            className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600 w-36"
+            onClick={() => setAddFriend(!addFriend)}
+          >
+            Add Friend
+          </button>
+        </div>
+      )}
     </>
   );
 }
