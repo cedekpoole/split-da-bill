62539099741f4dd94979d7a29d27abc049bfa2db
cedekpoole <cameron.edek.poole@gmail.com>
2024-10-05 13:25:11 +0100

    create fake friend data + add heading

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,36 @@
+import { useState } from "react";
 import FriendForm from "./components/FriendForm";
 
+const fakeFriends = [
+  {
+    id: 1,
+    name: "John Doe",
+    image: "https://randomuser.me/api",
+  },
+  {
+    id: 2,
+    name: "Jane Doe",
+    image: "https://randomuser.me/api",
+  },
+  {
+    id: 3,
+    name: "John Smith",
+    image: "https://randomuser.me/api",
+  },
+  {
+    id: 4,
+    name: "Jane Smith",
+    image: "https://randomuser.me/api",
+  },
+];
+
 function App() {
+  const [friends, setFriends] = useState(fakeFriends);
+
   return (
     <>
-      <p>Hello</p>
-      <FriendForm />
+      <h1 className="text-2xl font-bold text-center my-4">Split Da Bill</h1>
+      <FriendForm friends={friends} setFriends={setFriends} />
     </>
   );
 }
