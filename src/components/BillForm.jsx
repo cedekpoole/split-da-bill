5214b625c86a2628f331c7106897a32bd4b5da84
cedekpoole <cameron.edek.poole@gmail.com>
2024-10-06 14:29:01 +0100

    build bill form

--- src/components/BillForm.jsx
+++ src/components/BillForm.jsx
@@ -1,7 +1,86 @@
+import { useState } from "react";
+
 export default function BillForm() {
+  const [billTotal, setBillTotal] = useState(0);
+  const [ownExpense, setOwnExpense] = useState(0);
+  const [paid, setPaid] = useState(null);
+
+  const onFormSubmit = (e) => {
+    e.preventDefault();
+    if (ownExpense > billTotal) {
+      return;
+    } else if (paid === null) {
+      return;
+    }
+  };
+
   return (
-    <div>
-      <h1>Bill Form</h1>
+    <div className="flex flex-col items-center">
+      <h1 className="my-4">Bill Form</h1>
+      <form className="flex flex-col gap-4">
+        <div className="flex justify-between items-center">
+          <label htmlFor="bill-total" className="mr-4">
+            Total Bill
+          </label>
+          <input
+            type="number"
+            name="bill-total"
+            id="bill-total"
+            className="border rounded px-4 py-2"
+            placeholder="Enter bill total"
+            value={billTotal}
+            onChange={(e) => setBillTotal(e.target.value)}
+            required
+          />
+        </div>
+        <div className="flex justify-between items-center">
+          <label htmlFor="own-expense" className="mr-4">
+            Your expense
+          </label>
+          <input
+            type="number"
+            name="own-expense"
+            id="own-expense"
+            className="border rounded px-4 py-2"
+            placeholder="Enter your expense"
+            value={ownExpense}
+            onChange={(e) => setOwnExpense(e.target.value)}
+            required
+          />
+        </div>
+        <div className="flex justify-between items-center">
+          <label htmlFor="friend-expense" className="mr-4">
+            Their expense
+          </label>
+          <input
+            type="number"
+            name="friend-expense"
+            id="friend-expense"
+            className="border rounded px-4 py-2"
+            value={billTotal - ownExpense}
+            readOnly
+          />
+        </div>
+        <div className="flex justify-between items-center">
+          <label htmlFor="dropdown" className="mr-4">
+            Who paid?
+          </label>
+          <select
+            id="dropdown"
+            onChange={(e) => setPaid(e.target.value)}
+            required
+          >
+            <option value="true">You</option>
+            <option value="false">Friend</option>
+          </select>
+        </div>
+        <button
+          className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600 w-36"
+          onClick={onFormSubmit}
+        >
+          Split the bill
+        </button>
+      </form>
     </div>
   );
 }
